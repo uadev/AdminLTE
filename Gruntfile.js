81540e03aa7b38c099e0749b463eecfdd99d8f1e
Yuriy Voziy <yuriy.voziy@gettyimages.com>
2015-11-19 16:09:36 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+// AdminLTE Gruntfile
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.initConfig(require('config').grunt);
+
+  // Load all grunt tasks
+
+  // LESS Compiler
+  grunt.loadNpmTasks('grunt-contrib-less');
+  // Watch File Changes
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Compress JS Files
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Include Files Within HTML
+  grunt.loadNpmTasks('grunt-includes');
+  // Optimize images
+  grunt.loadNpmTasks('grunt-image');
+  // Validate JS code
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Delete not needed files
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  // Lint CSS
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  // Lint Bootstrap
+  grunt.loadNpmTasks('grunt-bootlint');
+
+  // Linting task
+  grunt.registerTask('lint', ['jshint', 'csslint', 'bootlint']);
+
+  //Server task
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  //Available tasks
+  grunt.loadNpmTasks('grunt-available-tasks');
+
+
+  // The default task (running "grunt" in console) is "watch"
+  grunt.registerTask('default', ['availabletasks']);
+  grunt.registerTask('start', ['connect:server', 'watch']);
+};
